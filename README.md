4228b8fe9e83ba737af2379cf1ab8784ecf9bf7b
Deepankar Pant <91065406+deepankarpant15@users.noreply.github.com>
2024-02-29 17:16:17 +0530

    Portfolio

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # PORTFOLIO
- 
+
+This is a portfolio website built with Next.js and Tailwind CSS. It showcases your skills, projects, and achievements in an elegant and responsive design.
+Engaging User Experience: Crafted with modern design principles and intuitive navigation for a seamless browsing experience.
+Showcase of Projects: Highlighting a diverse range of projects with detailed descriptions, technologies used, and interactive demos.
+Personal Branding: Reflecting professionalism and personality through customized sections, including an about me, skills, and contact information.
+Responsive Design: Ensuring compatibility across various devices, from desktops to smartphones, for accessibility and convenience.
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/deepankarpant15/PORTFOLIO.git`
+2. Navigate to the project directory: `cd portfolio-website`
+3. Install the dependencies: `npm install`
+
+## Usage
+
+1. Start the development server: `npm run dev`
+2. Open your browser and visit `http://localhost:3000` to view the website.
+
+## Dependencies
+
+The following dependencies are required for this project:
+
+- Next.js: A React framework for server-side rendering and static site generation.
+- Tailwind CSS: A highly customizable CSS framework.
+- React: A JavaScript library for building user interfaces.
+- React Icons: A collection of popular icons for React projects.
+- TypeScript: A typed superset of JavaScript that compiles to plain JavaScript.
+- Resend: Resend is the email API for developers.
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT). Feel free to use, modify, and distribute the code as per the terms of the license.
+
+## REFRENCE REPO :https://github.com/judygab/nextjs-portfolio/tree/main
---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,86 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>Mongodb</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>SRM University, Delhi NCR</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I have consistently demonstrated a strong passion for technology and
+            a drive for excellence in my academic pursuits. With a solid
+            foundation in programming, algorithms, and problem-solving, I am
+            eager to apply my knowledge and skills to real-world challenges. I
+            have actively participated in coding competitions, collaborated on
+            team projects, and honed my communication abilities. My goal is to
+            contribute to innovative solutions and make a meaningful impact in
+            the field of computer science.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            ></TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
